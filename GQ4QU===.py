32dce12d48589e37c511c205647b763e4e5a9a58
ZiaMenna <ZiaMenna@users.noreply.github.com>
2017-09-18 05:25:39 +0200

    Update GQ4QU===.py

--- GQ4QU===.py
+++ GQ4QU===.py
@@ -6,6 +6,5 @@ for num1 in primerange(1000, 10000):
     while num3 < 10000:
         num2 = nextprime(num2)
         num3 = num2 + ( num2 - num1)
-        if isprime(num3):
-			if sorted(str(num1))==sorted(str(num2))==sorted(str(num3)):
+        if isprime(num3) and sorted(str(num1)) == sorted(str(num2)) == sorted(str(num3)):
             print(str(num1)+str(num2)+str(num3))
